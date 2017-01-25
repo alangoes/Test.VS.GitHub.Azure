1d5b4128ca23ac57bb1426b4d430fa7e6e020016
alang <alan.c.goes@gmail.com>
2017-01-25 21:39:05 +0000

    First commit.

---
+++ Web.Test.VS.GitHub.Azure/Web.Test.VS.GitHub.Azure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web.Test.VS.GitHub.Azure.MvcApplication" Language="C#" %>
---
+++ Web.Test.VS.GitHub.Azure/Web.Test.VS.GitHub.Azure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Test.VS.GitHub.Azure.Startup))]
+namespace Web.Test.VS.GitHub.Azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
